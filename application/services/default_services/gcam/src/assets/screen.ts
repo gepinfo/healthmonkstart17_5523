f495dccbb9d39298dd682cccf0dad7f4532fe15f
gepinfo <info@geppettosoftware.com>
2022-06-30 12:06:15 +0000

    code commited from geppetto!

---
+++ application/services/default_services/gcam/src/assets/screen.ts
@@ -0,0 +1,13 @@
+    
+    export const resourcetypes = [
+    {"resources":"home", "role":"Guest"}
+    ,
+{"resources":"admin", "role":"Guest"}
+    ,
+{"resources":"sefscreen", "role":"Guest"}
+    ,
+{"resources":"Person Details", "role":"Guest"}
+    ,
+{"resources":"BMI Details", "role":"Guest"}
+    
+]
---
+++ application/services/default_services/systemcredentialmanager/src/credentials/securecredentials.ts
@@ -0,0 +1,4 @@
+export let credentials:any = {
+    DB_AUTH_healthmonkstart17_5523: 'mongodb://admin:password@',
+    DB_COLL_healthmonkstart17_5523: ':27017/healthmonkstart17_5523?authSource=admin'
+}
